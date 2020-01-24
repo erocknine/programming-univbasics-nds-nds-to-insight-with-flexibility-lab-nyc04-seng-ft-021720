994158f33eefb73dbfda790dba5cf47c6223419e
erocknine <echeungsk@gmail.com>
2020-01-24 22:09:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -81,7 +81,7 @@ end
   #
   # RETURN:
   #
-  # Array of Arrays containing all of a director's movies. Each movie will need
+  # Array of Hashes containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
 
 def movies_with_directors_set(source)
@@ -90,8 +90,8 @@ def movies_with_directors_set(source)
 
 
 
-
-
+  
+  directors_movies
 end
 
 # ----------------    End of Your Code Region --------------------
